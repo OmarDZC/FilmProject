78e3fa3f2458882ecac2a7142d3480826fdd9bcc
Omarki <omardzc94@gmail.com>
2025-09-30 21:28:29 +0200

    Aplicar .gitignore y limpiar repo

--- angular-peliculas/src/app/compartidos/componentes/selector-multiple/SelectorMultipleModelo.ts
+++
@@ -1,4 +0,0 @@
-export interface SelectorMultipleDTO {
-    llave: number,
-    valor: string //llave: id: 1, valor: genero: shonen... 
-}
--- angular-peliculas/src/app/compartidos/proveedores/proveedores.ts
+++
@@ -1,3 +0,0 @@
-import { InjectionToken } from "@angular/core";
-
-export const SERVICIO_CRUD_TOKEN = new InjectionToken("SERVICIO_CRUD_TOKEN")
